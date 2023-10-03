312107c06e165f9878c4b646132693aa80b0f5dd
Camden Drake Vaught <camden@Camdens-MBP.lan>
2023-10-03 15:04:05 -0700

    I think I am done finally

--- src/App.js
+++ src/App.js
@@ -37,129 +37,136 @@ class App extends Component {
 
   shuffleImages(images) {
     for (let i = 0; i < images.length; i++) {
-        let rnd = Math.floor(Math.random() * images.length);
-        [images[i], images[rnd]] = [images[rnd], images[i]];
+      let rnd = Math.floor(Math.random() * images.length);
+      [images[i], images[rnd]] = [images[rnd], images[i]];
     }
-}
+  }
 
-renderCard(i) {
-  const image = (this.state.images[i] == null) ? 'none' : 
-      ( this.state.firstPick == i || this.state.secondPick == i) ? 
-      'url(' + this.state.imagePath + this.state.images[i] + ')' : 
-      'url(' + this.state.imagePath + 'black_back.jpg)';
-  const enabled = (this.state.images[i] != null && 
+  renderCard(i) {
+    const image = (this.state.images[i] === null) ? 'none' : 
+        (( this.state.firstPick === i || this.state.secondPick === i) ? 
+        'url(' + this.imagePath + this.state.images[i] + ')' : 
+        'url(' + this.imagePath + 'black_back.jpg)');
+    /*const image = (this.state.images[i] == null) ? 'none' :
+      (this.state.firstPick == i || this.state.secondPick == i) ?
+        'url(' + this.imagePath + this.state.images[i] + ')' :
+        'url(' + this.imagePath + 'black_back.jpg)';*/
+    const enabled = (this.state.images[i] != null &&
       (i != this.state.firstPick && i != this.state.secondPick) &&
       (this.state.firstPick == -1 || this.state.secondPick == -1) &&
       (this.state.matches < 10)) ? true : false;
-  const eventHandler = (enabled)? this.handleClick: () => {};
-  const cursor = (enabled) ? "pointer" : "none";
-  const style = { backgroundImage: image,
-    cursor: cursor
+    const eventHandler = (enabled) ? this.handleClick : () => {};
+    const cursor = (enabled) ? "pointer" : "none";
+    const style = {
+      backgroundImage: image,
+      cursor: cursor
     }
-  const output = 
-      <div id={i} key={i} 
-          name="card" 
-          className="col-sm-2 card"
-          style={style}
-          onClick={eventHandler}
-          >&nbsp;
+    const output =
+      <div id={i} key={i}
+        name="card"
+        className="col-sm-2 card"
+        style={style}
+        onClick={eventHandler}
+      >&nbsp;
       </div>
 
-      
-  ;
-  return output;
-}
 
-render() {
-  let status = (this.state.matches < 10) ?
+      ;
+    return output;
+  }
+
+  render() {
+    let status = (this.state.matches < 10) ?
       'Matches: ' + this.state.matches + " Tries: " + this.state.tries :
       "Congratulations!  You found all 10 matches in " + this.state.tries + " tries!";
 
-  return (
-    <div>
-         <div className="container" id="board">
-              <div className="pb-2" id="status">{status}</div>
-              <div className="row">
-                  <div className="col-sm-1"></div>
-                  {this.renderCard(0)}
-                  {this.renderCard(1)}
-                  {this.renderCard(2)}
-                  {this.renderCard(3)}
-                  {this.renderCard(4)}
-                  <div class="col-1"></div>
-              </div>
-              <div className="row">
-                  <div className="col-sm-1"></div>
-                  {this.renderCard(5)}
-                  {this.renderCard(6)}
-                  {this.renderCard(7)}
-                  {this.renderCard(8)}
-                  {this.renderCard(9)}
-                  <div className="col-1"></div>
-              </div>
-              <div className="row">
-                  <div className="col-sm-1"></div>
-                  {this.renderCard(10)}
-                  {this.renderCard(11)}
-                  {this.renderCard(12)}
-                  {this.renderCard(13)}
-                  {this.renderCard(14)}
-                  <div className="col-1"></div>
-              </div>
-              <div className="row">
-                  <div className="col-sm-1"></div>
-                  {this.renderCard(15)}
-                  {this.renderCard(16)}
-                  {this.renderCard(17)}
-                  {this.renderCard(18)}
-                  {this.renderCard(19)}
-                  <div className="col-1"></div>
-              </div>
+    return (
+      <div>
+        <div className="container" id="board">
+          <div className="pb-2" id="status">{status}</div>
+          <div className="row">
+            <div className="col-sm-1"></div>
+            {this.renderCard(0)}
+            {this.renderCard(1)}
+            {this.renderCard(2)}
+            {this.renderCard(3)}
+            {this.renderCard(4)}
+            <div className="col-1"></div>
           </div>
+          <div className="row">
+            <div className="col-sm-1"></div>
+            {this.renderCard(5)}
+            {this.renderCard(6)}
+            {this.renderCard(7)}
+            {this.renderCard(8)}
+            {this.renderCard(9)}
+            <div className="col-1"></div>
+          </div>
+          <div className="row">
+            <div className="col-sm-1"></div>
+            {this.renderCard(10)}
+            {this.renderCard(11)}
+            {this.renderCard(12)}
+            {this.renderCard(13)}
+            {this.renderCard(14)}
+            <div className="col-1"></div>
+          </div>
+          <div className="row">
+            <div className="col-sm-1"></div>
+            {this.renderCard(15)}
+            {this.renderCard(16)}
+            {this.renderCard(17)}
+            {this.renderCard(18)}
+            {this.renderCard(19)}
+            <div className="col-1"></div>
+          </div>
+        </div>
 
-          </div>
-      );  
+      </div>
+    );
   }
 
-  
+  handleClick(event) {
+    console.log("Hey! You Clicked!")
+    const index = parseInt(event.target.id);
+    if (this.state.firstPick === -1)
+      this.setState({firstPick: index})
+    else {
+      this.setState({secondPick: index})
+      setTimeout(this.checkCards, 2000);
+    }
+    console.log(event.target.id);
+    
+
+  }
+
   isMatch() {
     if (this.state.images[this.state.firstPick].substr(4, 1) == this.state.images[this.state.secondPick].substr(4, 1))
-        return true;
+      return true;
     else
-        return false;
-}
+      return false;
+  }
 
-checkCards() {
-  let result = {...this.state};
+  checkCards() {
+    let result = { ...this.state};
     result.tries++;
     if (this.isMatch()) {
-        result.matches ++;
-        result.images[result.firstPick] = null;
-        result.images[result.secondPick] = null;
+      result.matches++;
+      result.images[result.firstPick] = null;
+      result.images[result.secondPick] = null;
     }
     result.firstPick = -1;
     result.secondPick = -1;
 
     this.setState({
-      images: result.images, 
-      firstPick: result.firstPick, 
+      images: result.images,
+      firstPick: result.firstPick,
       secondPick: result.secondPick,
-       matches: result.matches, 
-       tries: result.tries
-      });
-}
-
-handleClick(event) {    
-  const index = parseInt(event.target.id);
-
-  if (this.state.firstPick == -1)
-      this.state.firstPick = index;
-  else {
-      this.state.secondPick = index;
-      setTimeout(this.checkCards, 2000);
+      matches: result.matches,
+      tries: result.tries
+    });
   }
 
-}
 
 }
 
